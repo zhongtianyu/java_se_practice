ba25e744ca7d1f050523f36afece64d710ed70a4
karanda <1913398402@qq.com>
2019-11-25 23:13:04 +0800

    添加test函数

--- system_inout_demo/src/main/java/gdpu/karanda/SystemInOutDemo.java
+++ system_inout_demo/src/main/java/gdpu/karanda/SystemInOutDemo.java
@@ -2,7 +2,12 @@ package gdpu.karanda;
 
 public class SystemInOutDemo {
 
+    private static void test() {
+        System.out.println("hello git");
+    }
+
     public static void main(String[] args) {
+        test();
         System.out.println("hello world!!!");
     }
 
